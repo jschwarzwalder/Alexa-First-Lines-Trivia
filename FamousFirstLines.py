9caa3bcc68b9563a81eecebc2ab3bb2b73a02c2f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-06-17 23:16:49 -0700

    Standardized Card Title, added start over intent

--- FamousFirstLines.py
+++ FamousFirstLines.py
@@ -12,7 +12,7 @@ import random
 
 # --------------- Helpers that build all of the responses ----------------------
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
+def build_speechlet_response(output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
             'type': 'PlainText',
@@ -20,8 +20,8 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': 'Famous First Lines',
+            'content': output
         },
         'reprompt': {
             'outputSpeech': {
@@ -49,7 +49,6 @@ def select_question( list_of_questions ):
     """
 
     session_attributes = {}
-    #card_title = "First Line"
     
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
@@ -76,168 +75,8 @@ def help_prompt():
     return speech_output
 
 
+def next_question(speech_output):
 
-    
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "Thank you for playing Famous First Lines. " \
-                    "We hope that you have a good day! "
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-def check_answer(response_title, response_author, session):
-
-    correct_title = session['attributes']['correct_title']
-    correct_author = session['attributes']['correct_author']
-    
-    title_is_correct = False
-    author_is_correct = False
-    
-    
-    if response_title != None: 
-        title_is_correct = correct_title.lower() == response_title.lower()
-    if response_author != None:
-        author_is_correct = correct_author.lower() == response_author.lower()
-    
-    session_speech_output = ""
-    
-    if title_is_correct:
-        session_speech_output = "You guessed it was from " + response_title + ". Congratulations, that is correct. "
-    elif response_title != None:
-        session_speech_output = "You guessed it was from " + response_title + ", but the first line was actually from " + correct_title + ". "
-    else:
-        session_speech_output = "This line was from " + correct_title + ". "
-        
-    if author_is_correct:
-        session_speech_output += "You guessed the author was " + response_author + ". Congratulations, that is correct. "
-    elif response_author != None:
-        session_speech_output += "You guessed the author was " + response_author + ", but actually " + correct_author + " wrote " + correct_title + "."
-    else:
-        session_speech_output += "This line was from " + correct_title + " by " + correct_author + ". "
-        
-    return session_speech_output
-    
-def dont_know(intent, session):
-
-    card_title = intent['name']
-    session_attributes = session['attributes']
-
-
-    response_title = session['attributes'].get('response_title', None)
-    response_author = session['attributes'].get('response_author', None)
-    
-    speech_output = check_answer(response_title, response_author, session)
-    should_end_session = True
-    reprompt_text = "Say next to get a new line"
-                        
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-    
-def repeat_question(intent, session):
-
-    card_title = intent['name']
-    session_attributes = session['attributes']
-    should_end_session = False
-    
-    
-    # Repeat first line text if RepeatIntent
-    speech_output = session['attributes']['first_line']
-    
-    reprompt_text = "What is this from, and who is the author?"
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-        
-def help_intent(intent, session):
-
-    card_title = intent['name']
-    session_attributes = session['attributes']
-    should_end_session = False
-    
-    
-    # Read help text to user
-    speech_output = help_prompt()
-    
-    reprompt_text = session_attributes['first_line']
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-        
-           
-
-def answer_question(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-    card_title = intent['name']
-    session_attributes = session['attributes']
-    should_end_session = False
-
-    response_title = session['attributes'].get('response_title', None)
-    response_author = session['attributes'].get('response_author', None)
-    if 'value' in intent['slots']['Title']:
-        response_title = intent['slots']['Title']['value']
-        
-    if 'value' in intent['slots']['Author']:
-        response_author = intent['slots']['Author']['value']
-    
-    session_attributes['response_title'] = response_title
-    session_attributes['response_author'] = response_author
-    
-    # No author or title
-    if response_title == None and response_author == None:
-        speech_output = "I did not hear your response. " \
-                        "Please say my answer is the Title, by the Author. "
-        reprompt_text = "I'm sorry, I still can't hear you. " \
-                        "Please say my answer is the Title, by the Author."
-    # Author, but no title
-    elif response_title == None:
-        speech_output = "You said this was from a book by " + response_author + \
-                        ". Please say the title is Title."
-        reprompt_text = "If you know the title please say " \
-                        "The title is Title, if not, say I don't know"
-    # Title, but no author
-    elif response_author == None:
-        speech_output = "You said this was from " + response_title + \
-                        ". Please say it is by Author."
-        reprompt_text = "If you know the author please say " \
-                        "The author is author, if not, say I don't know."
-    # Title and author
-    else:
-        
-        speech_output = check_answer(response_title, response_author, session)
-        should_end_session = True
-        reprompt_text = "Say next to get a new line"
-                        
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-# --------------- Events ------------------
-
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
-
-
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # Dispatch to your skill's launch
-    speech_output = "Welcome. I will read you the first line of a book or poem. Tell me the title and author. "
-    # pull in questions
-    
     list_of_questions = [
         {
             "first_line": "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair... ",
@@ -306,7 +145,6 @@ def on_launch(launch_request, session):
         },
     ]
     
-    card_title = "First Line Triva"
     session_attributes = select_question(list_of_questions)
     should_end_session = False
     
@@ -317,9 +155,170 @@ def on_launch(launch_request, session):
      
     
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        speech_output, reprompt_text, should_end_session))
     
+def start_over(session, intent):
+    return next_question("")
+    
+def handle_session_end_request():
+    speech_output = "Thank you for playing Famous First Lines. " \
+                    "We hope that you have a good day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        speech_output, None, should_end_session))
 
+
+def create_favorite_color_attributes(favorite_color):
+    return {"favoriteColor": favorite_color}
+
+def check_answer(response_title, response_author, session):
+
+    correct_title = session['attributes']['correct_title']
+    correct_author = session['attributes']['correct_author']
+    
+    title_is_correct = False
+    author_is_correct = False
+    
+    
+    if response_title != None: 
+        title_is_correct = correct_title.lower() == response_title.lower()
+    if response_author != None:
+        author_is_correct = correct_author.lower() == response_author.lower()
+    
+    session_speech_output = ""
+    
+    if title_is_correct:
+        session_speech_output = "You guessed it was from " + response_title + ". Congratulations, that is correct. "
+    elif response_title != None:
+        session_speech_output = "You guessed it was from " + response_title + ", but the first line was actually from " + correct_title + ". "
+    else:
+        session_speech_output = "This line was from " + correct_title + ". "
+        
+    if author_is_correct:
+        session_speech_output += "You guessed the author was " + response_author + ". Congratulations, that is correct. "
+    elif response_author != None:
+        session_speech_output += "You guessed the author was " + response_author + ", but actually " + correct_author + " wrote " + correct_title + "."
+    else:
+        session_speech_output += "This line was from " + correct_title + " by " + correct_author + ". "
+        
+    return session_speech_output
+    
+def dont_know(intent, session):
+
+    session_attributes = session['attributes']
+
+
+    response_title = session['attributes'].get('response_title', None)
+    response_author = session['attributes'].get('response_author', None)
+    
+    speech_output = check_answer(response_title, response_author, session)
+    should_end_session = True
+    reprompt_text = "Say next to get a new line"
+                        
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, reprompt_text, should_end_session))
+
+    
+def repeat_question(intent, session):
+
+    session_attributes = session['attributes']
+    should_end_session = False
+    
+    
+    # Repeat first line text if RepeatIntent
+    speech_output = session['attributes']['first_line']
+    
+    reprompt_text = "What is this from, and who is the author?"
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, reprompt_text, should_end_session))
+        
+def help_intent(intent, session):
+
+    session_attributes = session['attributes']
+    should_end_session = False
+    
+    
+    # Read help text to user
+    speech_output = help_prompt()
+    
+    if 'first_line' in session_attributes:
+        reprompt_text = session_attributes['first_line']
+    else:
+        reprompt_text = "Say new game to begin playing."
+    
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, reprompt_text, should_end_session))
+        
+           
+
+def answer_question(intent, session):
+    """ Sets the color in the session and prepares the speech to reply to the
+    user.
+    """
+    session_attributes = session['attributes']
+    should_end_session = False
+
+    response_title = session['attributes'].get('response_title', None)
+    response_author = session['attributes'].get('response_author', None)
+    if 'value' in intent['slots']['Title']:
+        response_title = intent['slots']['Title']['value']
+        
+    if 'value' in intent['slots']['Author']:
+        response_author = intent['slots']['Author']['value']
+    
+    session_attributes['response_title'] = response_title
+    session_attributes['response_author'] = response_author
+    
+    # No author or title
+    if response_title == None and response_author == None:
+        speech_output = "I did not hear your response. " \
+                        "Please say my answer is the Title, by the Author. "
+        reprompt_text = "I'm sorry, I still can't hear you. " \
+                        "Please say my answer is the Title, by the Author."
+    # Author, but no title
+    elif response_title == None:
+        speech_output = "You said this was from a book by " + response_author + \
+                        ". Please say the title is Title."
+        reprompt_text = "If you know the title please say " \
+                        "The title is Title, if not, say I don't know"
+    # Title, but no author
+    elif response_author == None:
+        speech_output = "You said this was from " + response_title + \
+                        ". Please say it is by Author."
+        reprompt_text = "If you know the author please say " \
+                        "The author is author, if not, say I don't know."
+    # Title and author
+    else:
+        
+        speech_output = check_answer(response_title, response_author, session)
+        should_end_session = True
+        reprompt_text = "Say next to get a new line"
+                        
+    return build_response(session_attributes, build_speechlet_response(
+        speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    speech_output = "Welcome. I will read you the first line of a book or poem. Tell me the title and author.\n\n "
+    # pull in questions
+    return next_question(speech_output)
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
@@ -339,7 +338,7 @@ def on_intent(intent_request, session):
         return repeat_question(intent, session)
     elif intent_name == "DontKnowIntent":
         return dont_know(intent, session)
-    elif intent_name == "StartOverIntent":
+    elif intent_name == "AMAZON.StartOverIntent":
         return start_over(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return help_intent(intent, session)
